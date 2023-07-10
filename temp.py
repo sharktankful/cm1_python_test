e81a67059f61cf8cf2826dced02db31d9a1730e4
Alec Smith <alecmsmith18@gmail.com>
2023-07-10 17:32:12 -0500

    updated temp.py

--- temp.py
+++ temp.py
@@ -1,13 +1,44 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
+# TEMP
+from langchain.memory import ChatMessageHistory
+from langchain.schema import HumanMessage, SystemMessage
 
-chat = ChatOpenAI(openai_api_key="GRAB API KEY",
-                  model='gpt-3.5-turbo', client='my_applicaiton')
+history = ChatMessageHistory()
 
-messages = [
-    SystemMessage(content="Your an expert data scientist"),
-    HumanMessage(content="Why is the sky blue?")
-]
-response = chat(messages)
+# Function to send message and get a response from chatbot
+def send_message(message):
+    chat = ChatOpenAI(openai_api_key="",
+                    model='gpt-3.5-turbo', client='my_applicaiton')
 
-print(response.content, end='\n')
+    messages = [
+        SystemMessage(content="Your a helpful AI Assistant"),
+        HumanMessage(content=message)
+    ]
+    response = chat(messages)
+
+    # TEMP
+    history.add_user_message(message)
+
+    return response.content
+
+
+# User-Message Variables
+user_message = ""
+
+# Chatbot will stay activated until user enters 'quit' into the terminal/command-prompt
+while user_message.lower() != "quit":
+
+    # Sends message to ChatBot
+    response = send_message(user_message)
+
+    # Extract the bot's reply from the response variable and prints it
+    bot_reply = response 
+    print("ChatBot:", bot_reply, end='\n')
+
+    # User message to send to ChatBot
+    user_message = input("You: ")
+
+# TEMP
+print(history.messages)
+
+print("ChatBot Turned Off.")
